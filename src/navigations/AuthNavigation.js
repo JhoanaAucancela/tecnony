749e7ab50aae147a6be032dae687235193345ebc
JhoanaAucancela <74837501+JhoanaAucancela@users.noreply.github.com>
2023-02-27 18:13:09 -0500

    documentos

--- src/navigations/AuthNavigation.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AuthStack from "./stacks/AuthStack";
-
-
-export default function AuthNavigation() {
-    return <AuthStack />
-}
--- src/utils/axiosErrorHandler.js
+++
@@ -1,8 +0,0 @@
-export default function errorHandler(err){
-    let error = err;
-
-    if(err.response && err.response.data.hasOwnProperty("message")){
-        error = err.response.data;
-    }
-    return new Error(error.message);
-}
